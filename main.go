92b02aab4d4a81ad92b65bb2ece8f9b6cda52778
Lieuwe Rooijakkers <lieuwerooijakkers@gmail.com>
2018-10-02 14:39:47 +0200

    improve trie printing

--- main.go
+++ main.go
@@ -16,6 +16,27 @@ func format(date *time.Time) string {
 	return date.Format(layout)
 }
 
+func printRecursive(t *Trie, indent, key string) {
+	fmt.Printf("%s%s:\n", indent, key)
+
+	for _, val := range t.Value {
+		fmt.Printf("%s  %s - ", indent, format(val.Begin))
+		if val.Recording() {
+			fmt.Print("RECORDING ")
+		} else {
+			fmt.Printf("%s ", format(val.End))
+		}
+		if len(val.Tags) > 0 {
+			fmt.Printf("tags: %s", strings.Join(val.Tags, ", "))
+		}
+		fmt.Println()
+	}
+
+	for key, child := range t.Children {
+		printRecursive(child, indent+"  ", key)
+	}
+}
+
 func main() {
 	args := os.Args[1:]
 
@@ -60,25 +81,8 @@ func main() {
 			fmt.Printf("recording: %s\n", strings.Join(path, "/"))
 		}
 
-		m := trie.Gather()
-		for key, values := range m {
-			if len(values) == 0 {
-				continue
-			}
-
-			fmt.Printf("%s:\n", key)
-			for _, val := range values {
-				fmt.Printf("   %s - ", format(val.Begin))
-				if val.Recording() {
-					fmt.Print("RECORDING ")
-				} else {
-					fmt.Printf("%s ", format(val.End))
-				}
-				if len(val.Tags) > 0 {
-					fmt.Printf("tags: %s", strings.Join(val.Tags, ", "))
-				}
-				fmt.Println()
-			}
+		for key, child := range trie.Children {
+			printRecursive(child, "", key)
 		}
 
 	case create:
